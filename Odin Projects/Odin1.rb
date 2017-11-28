f056ca16874f1228e995a15803f0688a10b8d3a5
Tyler Cooper <tylertccooper@gmail.com>
2017-11-27 22:00:03 -0800

    p1 comments

--- Odin Projects/Odin1.rb
+++ Odin Projects/Odin1.rb
@@ -1,9 +1,11 @@
+# Caesar Cipher takes a string and shifts all letters by some given number
+
 def caesar_cipher(str,inc)
   rtnstr = ""
   str.each_char do |c|
-    if /[a-zA-Z]/.match(c)
-      (inc%26).times {c.next!}
-      rtnstr << c[c.size-1]
+    if /[a-zA-Z]/.match(c) # We only want to shift letters
+      (inc%26).times {c.next!} # % 26 prevents the loop from running too long
+      rtnstr << c[c.size-1] # if the string passes z it becomes aa,ab... we just want the last char
     else
       rtnstr << c
     end
