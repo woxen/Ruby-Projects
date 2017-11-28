4957b31e3538a276ef2f4760d0d19be047ae3725
Tyler Cooper <tylertccooper@gmail.com>
2017-11-27 21:40:54 -0800

    Updating p1 test

---
+++ Odin Projects/Odin1.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(str,inc)
+  rtnstr = ""
+  str.each_char do |c|
+    if /[a-zA-Z]/.match(c)
+      (inc%26).times {c.next!}
+      rtnstr << c[c.size-1]
+    else
+      rtnstr << c
+    end
+  end
+  puts rtnstr
+end
+
+caesar_cipher("What a string!", 5)
+
+puts puts puts
